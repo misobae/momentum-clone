4d9f80cc523f7b94ee3a3b82fb91fdec355ec47b
misobae <misobae0731@gmail.com>
2022-02-24 08:25:45 +0900

    Feat: Add Clock

---
+++ clock.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector('#clock');
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+// 처음 페이지가 로드되었을 때에도 시계가 보일 수 있도록 getclock 함수를 호출한 다음
+// setInterval로 1초마다 실행시키기.
+getClock();
+setInterval(getClock, 1000);
