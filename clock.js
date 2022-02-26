8efefdd2f5598c23c52e8f62f6052f5e5aa0bae3
misobae <misobae0731@gmail.com>
2022-02-26 17:05:20 +0900

    Feat: Delete seconds

--- clock.js
+++ clock.js
@@ -4,8 +4,8 @@ function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  // const seconds = String(date.getSeconds()).padStart(2, '0');
+  clock.innerText = `${hours}:${minutes}`;
 }
 
 // 처음 페이지가 로드되었을 때에도 시계가 보일 수 있도록 getclock 함수를 호출한 다음
