bbf088d0c8143f647ffd6665d5a0d03601ca1085
misobae <misobae0731@gmail.com>
2022-02-27 21:53:44 +0900

    Feat: 로그인 전 To Do Container 숨김

--- greetings.js
+++ greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
+const toDoContainer = document.querySelector('.todo-container');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
@@ -11,6 +12,7 @@ function onLoginSubmit(event) {
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   clock.classList.remove(HIDDEN_CLASSNAME);
+  toDoContainer.classList.remove(HIDDEN_CLASSNAME);
   paintGreetings(username);
 }
 
@@ -26,5 +28,6 @@ if(savedUsername === null) {
   loginForm.addEventListener('submit', onLoginSubmit);
 } else {
   clock.classList.remove(HIDDEN_CLASSNAME);
+  toDoContainer.classList.remove(HIDDEN_CLASSNAME);
   paintGreetings(savedUsername);
 }
